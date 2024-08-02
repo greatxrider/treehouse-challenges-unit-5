e2bbc801546de685d9679a458ce90f8ea6c705a0
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-08-02 18:33:25 +0800

    async-await

--- asynchronous-programming-with-javascript/asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
+++ asynchronous-programming-with-javascript/asynchronous-programming-with-javascript/4- Exploring Async-Await/2-convert-promise-handling-to-async-await/start/js/async-await.js
@@ -4,11 +4,22 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getPeopleInSpace(url) {
+  const peopleResponse = await fetch(url);
+  const peopleJSON = await peopleResponse.json();
+  const profiles = peopleJSON.people.map(async person => {
+    const craft = perons.craft;
+    const profileResponse = await fetch(wikiUrl + person.name);
+    const profileJSON = await profileResponse.json();
+    return { ...profileJSON, craft };
+  });
 
+  return Promise.all(profiles);
+}
 
 // Generate the markup for each profile
 function generateHTML(data) {
-  data.map( person => {
+  data.map(person => {
     const section = document.createElement('section');
     peopleList.appendChild(section);
     // Check if request returns a 'standard' page from Wiki
@@ -31,7 +42,8 @@ function generateHTML(data) {
   });
 }
 
-btn.addEventListener('click', (event) => {
+btn.addEventListener('click', async (event) => {
   event.target.textContent = "Loading...";
-
+  const astros = await getPeopleInSpace(astrosUrl);
+  generateHTML(astros);
 });
